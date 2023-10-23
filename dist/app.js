e2e55bd99b978437a523c1d14fc92d5de188a783
PreeyanuchYu <babymoonshop@gmail.com>
2023-10-23 12:06:23 +0700

    Feat: Add List

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,16 @@
 "use strict";
 const inputEl = document.getElementById("input");
 const formEl = document.querySelector("form");
+const listEl = document.getElementById("list");
 formEl.addEventListener("submit", saveData);
 function saveData(e) {
     e.preventDefault();
-    console.log(inputEl.value);
+    const message = inputEl.value;
+    createList(message);
+}
+function createList(text) {
+    const liEl = document.createElement("li");
+    liEl.append(text);
+    listEl.append(liEl);
+    inputEl.value = "";
 }
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
-
 const inputEl = document.getElementById("input")! as HTMLInputElement
 const formEl = document.querySelector("form")!
+const listEl = document.getElementById("list")!
 
 formEl.addEventListener("submit",saveData)
 
 function saveData(e:SubmitEvent){
     e.preventDefault()
-    console.log(inputEl.value)
+    const message = inputEl.value
+    createList(message)
+}
+
+function createList(text:string){
+    const liEl = document.createElement("li")
+    liEl.append(text)
+    listEl.append(liEl)
+    inputEl.value = ""
 }
